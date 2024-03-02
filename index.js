72b0fd88f39e7c3aa7ca507b09fc799e86d24241
pratyushsingha <pratyushsingha83@gmail.com>
2024-03-02 09:39:45 +0530

    fix: fixed for connecting with serverless runtime

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import dotenv from "dotenv";
-
 import connectDB from "./src/db/index.js";
 import { app } from "./app.js";
 
@@ -7,8 +6,25 @@ dotenv.config({
   path: "./env",
 });
 
-connectDB().then(() => {
-  app.listen(process.env.PORT, () => {
-    console.log(`server is running at port : ${process.env.PORT}`);
+const PORT = process.env.PORT || 3000;
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(`Server is running at port: ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Error connecting to the database:", error);
+    process.exit(1);
+  }
+};
+
+connectDB()
+  .then(() => {
+    startServer();
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database:", error);
+    process.exit(1);  
   });
-});
