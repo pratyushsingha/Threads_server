45ec28ad3c1b44691cda31a1cc21b08ed9d011c3
pratyushsingha <pratyushsingha83@gmail.com>
2024-08-07 23:13:02 +0530

    Feat: added filter on Activities

--- src/routes/activity.route.js
+++ src/routes/activity.route.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { getAllActivities } from "../controllers/activity.controller.js";
+import { getActivities } from "../controllers/activity.controller.js";
 
 const router = Router();
 
-router.route("/").get(verifyJWT, getAllActivities);
+router.route("/").get(verifyJWT, getActivities);
 
 export default router;
